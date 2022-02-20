1f31f5893fac3a0e533450f6c118519d66d72ae8
MahadiHasancoder <mahadidev7@gmail.com>
2022-02-20 16:28:35 +0600

    Product alt Update

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,7 +7,7 @@ const Product = ({src, title}) => {
         <div className='product__item'>
             <a href='./'>
                 <div className='product__item_Img'>
-                    <img src={src}  alt='product'/>
+                    <img src={src}  alt={title}/>
                 </div>
 
                 <h3 className='plain_text .font-medium'>{title}</h3>
